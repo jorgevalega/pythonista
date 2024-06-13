4ee2edb8392c11c51d7f1f1bfb82558ee3930826
Jorge Valega <jorgevalega@gmail.com>
2024-06-13 00:00:44 -0300

    Update 02_01_variaveis.py
    Arquivo variaveis atualizado.

---
+++ 02_01_variaveis.py
@@ -0,0 +1,35 @@
+velocidade_internet = 10
+print(velocidade_internet)
+
+# Tipos de dados
+
+# Números
+velocidade_internet = 10
+
+# Boleanos
+esta_aberta = True
+
+# Strings
+slogan = 'Feito é melhor que perfeito!'
+frase = "Pose usar aspas duplas também"
+
+a,b,c,d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+print(type(velocidade_internet))
+
+# Desafio
+# Copie e cole as seguintes linhas de código para seu editor de código e descubra qual é o tipo de cada um deses datos
+variavel_1 = 25.78
+variavel_2 = True
+variavel_3 = 'Bom dia'
+variavel_4 = 15
+
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
