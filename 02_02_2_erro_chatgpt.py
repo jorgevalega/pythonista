dfdca513baebfda3bc6a1634113f2af0dbfffee3
Jorge Valega <jorgevalega@gmail.com>
2024-06-16 00:26:32 -0300

    adicionado arquivo 02_02_2_erro_chatgpt.py

---
+++ 02_02_2_erro_chatgpt.py
@@ -0,0 +1,15 @@
+# Erro genérico 
+# 1 - leia o erro
+# 2 - copie a mensagem e cole no tradutor
+# 3 - tente entender o que fazer a partir da mensagem
+# Se acostume a pesquisar em inglês
+
+nome = 'raquel'
+print(nome[5])
+
+
+# Erro específico (inclui dados da sua aplicação)
+with open('senhas.txt','r') as arquivo:
+    for linha in arquivo:
+        print(linha )
+# Só pesquise por a parte genérica e não a específica.       
