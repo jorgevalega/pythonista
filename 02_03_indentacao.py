644ad2a4c186b3587f367cd2092698c3f20542c1
Jorge Valega <jorgevalega@gmail.com>
2024-06-19 20:33:04 -0300

    Update 02_03_indentacao.py
    Update 02_03_indentacao.py

---
+++ 02_03_indentacao.py
@@ -0,0 +1,28 @@
+
+if 10 > 5:
+    print('10 é maior que 5')
+
+class BemVindo():
+    print('Bem Vindo!')
+
+oi = BemVindo()
+
+# Desafio
+
+import time
+
+
+def PensarPor10Segundos():
+      print('pensando')
+      time.sleep(10)
+      print('Lembrei!')
+
+
+if 10 > 5:
+    print('10 é maior que 5')
+
+class BemVindo():
+      def __init__(self):
+           print('Bem vindo')
+
+oi = BemVindo()
