1c884bf074d67316c8470e20303ed20b851577e7
Jorge Valega <jorgevalega@gmail.com>
2024-07-11 01:22:42 -0300

    Criação de arquivo
    Criação de arquivo

---
+++ 02_09_input.py
@@ -0,0 +1,10 @@
+senha = input('Digite sua senha: ')
+print(senha)
+print(type(senha))
+
+quantidade_filmes = int(input('Quantos filmes você já viu este mês? '))
+print(type(quantidade_filmes))
+
+# Para float pode usar o ponto no lugar da vírgula.
+preco_produto = float(input('Qual é o preço do produto? '))
+print(type(preco_produto))
