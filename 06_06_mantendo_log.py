26268d965404cc3f75ea4825ad051a2e3acfe492
Jorge Valega <jorgevalega@gmail.com>
2024-07-14 00:16:36 -0300

    Novos arquivos
    Novos arquivos

---
+++ 06_06_mantendo_log.py
@@ -0,0 +1,15 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='a', format='%(levelname)s - %(message)s - %(asctime)s' )
+
+
+
+try:
+    email = input('Digite seu e-mail: ')
+    senha = int(input('Digite sua senha bancária: '))
+    if senha == 1234:
+        print('Login feito com sucesso!')
+        logging.info(f'{email} entrou em sua conta bancária')
+except ValueError as erro:
+    print('Digite apenas números')
+    logging.info(erro)
