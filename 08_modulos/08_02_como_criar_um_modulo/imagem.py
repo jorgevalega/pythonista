771d4b4d41b2a7da23204cbe2359f50d66cd414a
Jorge Valega <jorgevalega@gmail.com>
2024-07-27 07:51:00 -0300

    Criação de arquivos
    Criação de arquivos

---
+++ 08_modulos/08_02_como_criar_um_modulo/imagem.py
@@ -0,0 +1,7 @@
+def tirar_foto():
+    print('Tirando foto')
+
+qualidade_maxima = 10
+
+class Camera:
+    pass
---
+++ 08_modulos/08_02_como_criar_um_modulo/video.py
@@ -0,0 +1,8 @@
+from imagem import tirar_foto,qualidade_maxima,Camera
+
+def iniciar_gravacao():
+    print('Iniciando a gravação')
+
+tirar_foto()
+print(qualidade_maxima)
+camera = Camera()
