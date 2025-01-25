8e155bd72bcc773d39b0342ef9154a76e52b8ec1
Jorge Valega <jorgevalega@gmail.com>
2025-01-25 10:34:40 -0300

    new files
    new files

---
+++ 02_dados/02_13_atalhos.py
@@ -0,0 +1,24 @@
+# Para comentar seu código
+# Ctrl + k + c (para comentar)
+# Ctrl + k + u (para descomentar o código)
+
+# Também pode comentar um bloco de código 
+# com Shift + Alt + a (para descomentar igual)
+
+""" Bloco de código
+Bloco de código
+Bloco de código
+Bloco de código
+Bloco de código """
+
+# Para mover o código para abaixo ou para cima, apertando 
+# Alt + seta para baixo ou para cima
+
+# Para duplicar linhas Alt + Shift + seta para baixo
+
+# Para ocultar a barra lateral do VS Code com Ctrl + B
+
+
+
+
+
